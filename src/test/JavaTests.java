e5d1cd40871065ff406015429d288feaaecd5276
lefrero <56884173+lefrero@users.noreply.github.com>
2021-11-09 14:32:24 +0100

    Add files via upload

---
+++ src/test/JavaTests.java
@@ -0,0 +1,25 @@
+package controlenov;
+
+import org.junit.jupiter.api.Test;
+
+public class JavaTests {
+	
+	bool x = true;
+	
+	@Test
+	 void dumbTest() {
+		assertFalse(x);
+	}
+	
+	/*
+	 * vrai test :
+	 * 
+	 * @test
+	 * void affectionTest(){
+	 * g.affecter(s);
+	 * assertTrue(g.sujetAffecté() == s || s.groupe() == g)
+	 *  
+	 * 
+	 * }
+	 */
+}
